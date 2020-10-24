830e81184ab70237fe6552bf345bd94a836df86c
Mauricio211 <71468645+Mauricio211@users.noreply.github.com>
2020-10-23 18:23:41 -0700

    Update Ejercicio_36.cpp

--- Programas/Ejercicio_36.cpp
+++ Programas/Ejercicio_36.cpp
@@ -2,12 +2,12 @@
 INSTITUTO TECNOLOGICO DE TIJUANA
 CHAVEZ ARROYO MAURICIO
 NO.CONTROL: 18212159
-/*
+*/
 /*36.-Calcular z de acuerdo con la formula siguiente.
 Asignar las variables numericas aprobadas.
 Sean a = 4, b = 6, x = 8, y = 2, c = 5 cuando
 corra su programa.
-/*
+*/
 #include "pch.h"
 #include <iostream>
 using namespace std;
