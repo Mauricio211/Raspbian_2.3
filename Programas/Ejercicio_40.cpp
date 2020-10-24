7c7f5359a767dee0622f8479dd20c791988689c4
Mauricio211 <71468645+Mauricio211@users.noreply.github.com>
2020-10-23 18:17:41 -0700

    Create Ejercicio_40.cpp

---
+++ Programas/Ejercicio_40.cpp
@@ -0,0 +1,18 @@
+/*
+INSTITUTO TECNOLOGICO DE TIJUANA
+CHAVEZ ARROYO MAURICIO
+NO.CONTROL: 18212159
+/*
+/*
+40.-Una pulgada equivale a 2.5cm.,calcular el numero de cm en 32 pulgadas.
+/*
+#include "pch.h"
+#include <iostream>
+using namespace std;
+double cm,in=32;
+int main()
+{
+	cm = in * 2.5;
+	cout << "32 pulgadas es igual a " << cm;
+	printf("cm");
+}
